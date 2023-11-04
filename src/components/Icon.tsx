cbd73e8e93566973fd3808eaf47f7b0195e11c7e
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-11-04 10:00:54 +0100

    chore: login attempt animation

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,14 +3,15 @@ import * as Icons from '@radix-ui/react-icons'
 interface IconProps {
   icon: string
   className?: string
+  onClick?: () => void
 }
 
-const Icon = ({ icon, className }: IconProps) => {
+const Icon = ({ icon, className, onClick }: IconProps) => {
   // @ts-expect-error-next-line
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, import/namespace
   const IconRadix = Icons[icon]
 
-  return <IconRadix className={className} />
+  return <IconRadix className={className} onClick={onClick} />
 }
 
 export default Icon
--- src/components/screen/LoginScreen.tsx
+++ src/components/screen/LoginScreen.tsx
@@ -1,13 +1,38 @@
 import { useState } from 'react'
 
+import useSystemStore from '@/stores/system-store'
+
 import avatar from '@/assets/login/avatar.gif'
 
 import { cn } from '@/utils/styles'
 
 import Icon from '../Icon'
 
+import { SystemScreen } from '@/types/store'
+
 const LoginScreen = () => {
   const [showLoginInput, setShowLoginInput] = useState(false)
+  const [validation, setValidation] = useState(false)
+  const [loginInput, setLoginInput] = useState('')
+  const { setScreen } = useSystemStore()
+
+  const loginAttempt = (
+    keyboardEvent?: React.KeyboardEvent<HTMLInputElement>,
+  ) => {
+    const isKeyEnter = keyboardEvent?.key === 'Enter'
+    const isPasswordValid = loginInput.length
+
+    if (isPasswordValid && (!keyboardEvent?.key || isKeyEnter)) {
+      return setScreen(SystemScreen.HOME)
+    } else if (isKeyEnter && !isPasswordValid) {
+      setValidation(true)
+
+      setTimeout(() => {
+        setValidation(false)
+      }, 500)
+    }
+  }
+
   return (
     <main
       className='flex h-screen w-screen flex-col items-center justify-center gap-8 bg-black/20 animate-in fade-in-0'
@@ -19,12 +44,28 @@ const LoginScreen = () => {
           Ziga Macele
         </span>
         <div className={cn('opacity-0', showLoginInput && 'opacity-100')}>
-          <input
-            className='w-40 rounded-full bg-white/40 py-1.5 pl-3 text-left font-["Verdana"] text-sm leading-3 text-white outline-none backdrop-blur-md animate-in fade-in placeholder:font-sans placeholder:text-[13px] placeholder:font-medium placeholder:text-white/40'
-            type='password'
-            placeholder='Enter Password'
-            maxLength={12}
-          />
+          <span
+            className={cn(
+              'relative flex items-center justify-center',
+              validation && 'inputWiggle',
+            )}
+          >
+            <input
+              className='w-40 rounded-full bg-white/40 py-1.5 pl-3 text-left font-["Verdana"] text-sm leading-3 text-white outline-none backdrop-blur-md animate-in fade-in placeholder:font-sans placeholder:text-[13px] placeholder:font-medium placeholder:text-white/40'
+              type='password'
+              placeholder='Enter Password'
+              maxLength={12}
+              onKeyDown={loginAttempt}
+              onChange={(e) => setLoginInput(e.target.value)}
+            />
+            {!!loginInput.length && (
+              <Icon
+                icon='ArrowRightIcon'
+                className='absolute right-1.5 top-1/2 h-5 w-5 -translate-y-1/2 cursor-pointer rounded-full border-[1.5px] p-0.5 opacity-60 hover:opacity-100'
+                onClick={loginAttempt}
+              />
+            )}
+          </span>
         </div>
         <span className='mt-0.5 cursor-pointer text-xs font-medium drop-shadow-lg transition-opacity hover:opacity-60'>
           Touch ID or Enter Password
