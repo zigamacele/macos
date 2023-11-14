376903eb78a6361fc2f5de75042d536edffdc24b
zigamacele <19758116+zigamacele@users.noreply.github.com>
2023-11-14 18:17:29 +0100

    chore: file structure

---
+++ src/constants/file-structure.ts
@@ -0,0 +1,65 @@
+import { username } from './system'
+
+enum FileType {
+  ROOT = 'ROOT',
+  FOLDER = 'FOLDER',
+}
+
+export interface FileStructure {
+  [key: string]: {
+    name: string
+    type: FileType
+    children?: FileStructure
+  }
+}
+
+export const fileStructure: FileStructure = {
+  root: {
+    name: 'Macintosh HD',
+    type: FileType.FOLDER,
+    children: {
+      applications: {
+        name: 'Applications',
+        type: FileType.FOLDER,
+        children: {},
+      },
+      library: {
+        name: 'Library',
+        type: FileType.FOLDER,
+        children: {},
+      },
+      system: {
+        name: 'System',
+        type: FileType.FOLDER,
+        children: {},
+      },
+      users: {
+        name: 'Users',
+        type: FileType.FOLDER,
+        children: {
+          [username]: {
+            name: username,
+            type: FileType.FOLDER,
+            children: {
+              desktop: {
+                name: 'Desktop',
+                type: FileType.FOLDER,
+                children: {},
+              },
+              documents: {
+                name: 'Documents',
+                type: FileType.FOLDER,
+                children: {},
+              },
+              downloads: {
+                name: 'Downloads',
+                type: FileType.FOLDER,
+                children: {},
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
--- src/constants/system.ts
+++ src/constants/system.ts
@@ -1,1 +1,1 @@
-export const username = 'zigamacele'
+export const username: string = 'zigamacele'
